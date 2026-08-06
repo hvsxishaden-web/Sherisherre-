0373ddeff6d314637e8bf72ade4738860d36be25
hvsxishaden-web <hvsxishaden@gmail.com>
2026-08-06 19:33:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import TelegramBot from 'node-telegram-bot-api';
 
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    `أهلًا وسهلًا 
+
+اكتب استفسارك، بإذن الله سأرد عليك في أقرب وقت.`
+  );
+});
+
+console.log('Bot is running...');
